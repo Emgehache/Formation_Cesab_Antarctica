39da722ffb3370fc986a58a03e8b196d17a0d2da
cdeslias <camille.deslias75@gmail.com>
2019-12-05 18:16:49 +0100

    loop and download ok

--- R/03_function_get_url_data.R
+++ R/03_function_get_url_data.R
@@ -1,11 +1,12 @@
 
 #library(lubridate)
+#library(ncdf4)
 
 #get_chl_a_data<- function(start_date, date_end) {
 ### Define periode to interest 
 
-start_date <- ymd("2017-01-08")
-end_date   <- ymd("2017-01-18")
+start_date <- ymd("2017-02-02")
+end_date   <- ymd("2017-02-27")
   
 # day of the year 
 yr         <- year(start_date) 
@@ -23,10 +24,23 @@ first_day_end_file <- floor((end_day-1)/step)*step + 1
 
 files=seq(first_day_start_file,first_day_end_file,by=8)
 
-url_base <- "https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/A"
+url_base <- "https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/"
 
 for(d in files) {
-  url <- paste0(url_base,yr,formatC(d,width=3,flag=0,format = "d"),
-                yr,formatC(d+7,width=3,flag=0,format = "d"),".L3m_8D_",variable,"4km.nc")
+  file_name <- paste0("A",yr,formatC(d,width=3,flag=0,format = "d"),
+                yr,formatC(d+7,width=3,flag=0,format = "d"),".L3m_8D_",variable,"_4km.nc")
+  url <- paste0(url_base,file_name)
+  if (!file.exists(paste0("./data/",file_name))){
+    
+    res <- tryCatch(download.file(url,
+                                  destfile=paste0("./data/",file_name),
+                                  method="auto"),
+                    error=function(e) 1)
+    # if(dat!=1) load(paste0("./data/",file_name))
+  }
+  
+    
+  
+  
 }
-# download.files(url)
+#download.file(url, des)
