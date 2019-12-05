e81270eed85d4b95f1d5a867cc2f41fa8d44c66a
Mathieu de Goër <mathieu.de-goer@protonmail.com>
2019-12-05 17:51:09 +0100

    Improvement of the get_url_data function

--- R/03_function_get_url_data.R
+++ R/03_function_get_url_data.R
@@ -1,11 +1,11 @@
 
-library(lubridate)
+#library(lubridate)
 
-get_chl_a_data<- function(start_date, date_end) {
+#get_chl_a_data<- function(start_date, date_end) {
 ### Define periode to interest 
 
-start_date <- ymd("2017-01-23")
-end_date   <- ymd("2017-02-02")
+start_date <- ymd("2017-01-08")
+end_date   <- ymd("2017-01-18")
   
 # day of the year 
 yr         <- year(start_date) 
@@ -18,15 +18,15 @@ variable <- "CHL_chlor_a" #"SST_sst"
 
 # find correct name for file in ocean color site 
 step <- 8 
-first_day_start_file <- floor(start_day/step) *step + 1
-first_day_end_file <- floor(end_day/step) *step + 1
+first_day_start_file <- floor((start_day-1)/step)*step + 1
+first_day_end_file <- floor((end_day-1)/step)*step + 1
 
-
-
-
+files=seq(first_day_start_file,first_day_end_file,by=8)
 
 url_base <- "https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/A"
 
-url <- paste0(url_base,yr,start_day,yr,end_date,".L3m_8D_",variable,"4km.nc")
-
-donwload.files(url)
+for(d in files) {
+  url <- paste0(url_base,yr,formatC(d,width=3,flag=0,format = "d"),
+                yr,formatC(d+7,width=3,flag=0,format = "d"),".L3m_8D_",variable,"4km.nc")
+}
+# download.files(url)
