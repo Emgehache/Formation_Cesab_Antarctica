091f519aa25b601d7d1ffaa7f800389575cc83c0
polarbear-tataki <ilanperez.perso@gmail.com>
2019-12-05 16:53:06 +0100

    Script fonction get_track

---
+++ R/01_function_getacetrack.R
@@ -0,0 +1,43 @@
+###################################
+###
+###   Fonction get_1hour_acetrack
+###   get FROM ZENODO track og the ship
+###
+###   
+##################################
+
+
+
+get_1hour_acetrack <- function(year, months) {
+  
+  ## Checks                         ----------
+  if (missing(year)) { stop("Missing year") }
+  if (!is.vector(months)) { stop("x must be a numeric vector.") }
+  if (!is.numeric(months)) { stop("x must be a numeric vector.") }
+  
+  
+  #### Create empty vector
+  c <- c()
+  
+  for (i in min(months):max(months)) {
+    
+    tryCatch({
+      print(i)
+    
+    url <- paste0("https://zenodo.org/record/3483164/files/ace_cruise_track_1hour_",year,"-","0",i,".csv?download=1")
+    
+    track <- paste0("data/track",year,"0",i,".csv")
+    download.file(url = url, destfile = track)
+    track<- read.csv(file = track)
+    
+    assign(paste0("track",year,"_","0",i), track)
+    
+    
+    
+            }, error=function(e){})
+    
+    
+  }
+}
+
+get_1hour_acetrack(year = 2017, months = c(1,2))
