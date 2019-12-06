c236489395f877748ce42030e6da3daad084173d
cdeslias <camille.deslias75@gmail.com>
2019-12-06 15:26:39 +0100

    final

--- R/06_function_transform_sst_nc_to_raster.R
+++ R/06_function_transform_sst_nc_to_raster.R
@@ -1,10 +1,12 @@
 #library(ncdf4) # package for netcdf manipulation
 #library(raster) # package for raster manipulation
 
-transform_nc_to_rasterlayer <- function(day1, day2) {
+transform_nc_to_rasterlayer <- function(day1, day2, variable, resolution) {
 day <- c(day1:day2)
+var <- variable #SST_sst #CHL_chlor_a
+res <- resolution #8D #DAY
 for(d in day) {
-  file_name  <- paste0("./data/A20170",d,".L3m_DAY_SST_sst_4km.nc")
+  file_name  <- paste0("./data/A20170",d,".L3m_",res,"_",var,"_4km.nc")
   SST_raster <- raster(file_name,
                           level=180,
                           varname= "sst")
---
+++ R/make_download.R
@@ -0,0 +1,5 @@
+get_url_env_data("2017-01-20", "2017-01-31", "CHL_chlor_a")
+get_url_env_data("2017-01-20", "2017-01-31", "SST_sst")
+
+get_url_env_data_day("2017-01-20", "2017-01-31", "CHL_chlor_a")
+get_url_env_data_day("2017-01-20", "2017-01-31", "SST_sst")
